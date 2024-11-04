d22906bbcaf7f40c752cee2e5a4fd77f6ad87ba2
Afroz233 <zackhan23@gmail.com>
2024-11-05 02:13:31 +0530

    added backend deployed url

--- frontend/src/utils/Path.js
+++ frontend/src/utils/Path.js
@@ -1,4 +1,4 @@
-const pathUrl = "http://localhost:4000/";
-// const pathUrl = "https://real-state-api-y6js.onrender.com/";
+// const pathUrl = "http://localhost:4000/";
+const pathUrl = "https://real-state-api-y6js.onrender.com/";
 
 export default pathUrl;
