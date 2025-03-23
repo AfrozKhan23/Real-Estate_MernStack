63bddd6facc01b0316bf000ad223e72af8c616cd
Afroz233 <zackhan23@gmail.com>
2025-03-23 17:41:36 +0530

    added3

--- backend/index.js
+++ backend/index.js
@@ -16,16 +16,8 @@ const PORT = process.env.PORT || 4000;
 // };
 
 // app.use(cors(corsOptions));
-app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://real-state-mern-stack.vercel.app",
-    "http://localhost:5173"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
+app.use(cors());
+
 app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ extended: true, limit: "100mb" }));
 connectDb();
