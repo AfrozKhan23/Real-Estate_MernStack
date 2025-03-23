8711fd080889f73a274b90b884757a90cec0907f
Afroz233 <zackhan23@gmail.com>
2025-03-23 17:25:56 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -9,15 +9,24 @@ import property from "./routes/property.routes.js";
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-const corsOptions = {
-  origin: ["https://real-state-mern-stack.vercel.app", "http://localhost:5173"],
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-};
+// const corsOptions = {
+//   origin: ["https://real-state-mern-stack.vercel.app", "http://localhost:5173"],
+//   methods: ["GET", "POST", "PUT", "DELETE"],
+//   allowedHeaders: ["Content-Type", "Authorization"],
+// };
 
-app.use(cors(corsOptions));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(cors(corsOptions));
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://real-state-mern-stack.vercel.app"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ extended: true, limit: "100mb" }));
 connectDb();
 
 app.get("/", (req, res) => {
